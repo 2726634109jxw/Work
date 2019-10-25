fb9fab280611e03eb70f26ca282d5042c86de759
jxw <2726634109@qq.com>
2019-10-25 10:57:07 +0800

    添加了一个方法

--- Hello_add/src/com/turing.java
+++ Hello_add/src/com/turing.java
@@ -1,6 +1,12 @@
 package com;
 
 public class turing {
+    public static void text(){
+        for (int i=0;i<10;i++){
+            System.out.println("这是dev中输出的一句话");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("hello java");
     }
